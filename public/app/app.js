c519ab3de8ec62615740617cff122102b30aaf7a
Rob Ellis <rob@chloi.io>
2013-11-10 15:46:36 -0800

    Initial commit

---
+++ public/app/app.js
@@ -0,0 +1,24 @@
+angular.module( 'ngHarpBoilerplate', [
+  'ngHarpBoilerplate.home',
+  'ngHarpBoilerplate.about',
+  'ui.state',
+  'ui.route'
+])
+
+.config( function myAppConfig ( $stateProvider, $urlRouterProvider ) {
+  $urlRouterProvider.otherwise( '/home' );
+})
+
+.run( function run () {
+})
+
+.controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
+  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+    if ( angular.isDefined( toState.data.pageTitle ) ) {
+      $scope.pageTitle = toState.data.pageTitle + ' | ngHarpBoilerplate' ;
+    }
+  });
+})
+
+;
+
